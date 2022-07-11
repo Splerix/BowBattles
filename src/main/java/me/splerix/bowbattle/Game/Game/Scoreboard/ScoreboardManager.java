f11cc8ebc528bf87ef256b4dc772d8a86abe2d4c
Splerix <splerixs@gmail.com>
2022-07-10 23:14:52 -0700

    Bug Fix

--- src/main/java/me/splerix/bowbattle/Game/Game/Scoreboard/ScoreboardManager.java
+++ src/main/java/me/splerix/bowbattle/Game/Game/Scoreboard/ScoreboardManager.java
@@ -1,15 +1,12 @@
 package me.splerix.bowbattle.Game.Game.Scoreboard;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
 
 import java.util.*;
 
 public class ScoreboardManager {
     public Map<UUID, ScoreboardInfo> scoreboardInfo = new HashMap<>();
-    int count = 0;
     private final List<UUID> displayBoardTo;
     private final int gameID;
     private final String mapName;
@@ -28,30 +25,14 @@ public class ScoreboardManager {
     }
 
     public void updateScoreboard() {
-        if (count == 4) count = 0;
         for (UUID uuid : displayBoardTo) {
             Player player = Bukkit.getPlayer(uuid);
             if (!(scoreboardInfo.containsKey(uuid))) {
                 scoreboardInfo.put(uuid, new ScoreboardInfo(gameID,  uuid, mapName));
                 player.setScoreboard(scoreboardInfo.get(uuid).getGameBoard());
             }
-            switch (count) {
-                case 0:
-                    player.getScoreboard().getObjective(DisplaySlot.SIDEBAR).setDisplayName(ChatColor.DARK_PURPLE + "BowBattle Game");
-                    break;
-                case 1:
-                    player.getScoreboard().getObjective(DisplaySlot.SIDEBAR).setDisplayName(ChatColor.AQUA + "BowBattle Game");
-                    break;
-                case 2:
-                    player.getScoreboard().getObjective(DisplaySlot.SIDEBAR).setDisplayName(ChatColor.DARK_BLUE + "BowBattle Game");
-                    break;
-                case 3:
-                    player.getScoreboard().getObjective(DisplaySlot.SIDEBAR).setDisplayName(ChatColor.GOLD + "BowBattle Game");
-                    player.setScoreboard(scoreboardInfo.get(uuid).getGameBoard());
-                    break;
-            }
+            player.setScoreboard(scoreboardInfo.get(uuid).getGameBoard());
         }
-        count++;
     }
     public void onGameEnd() {
         for (UUID uuid : displayBoardTo) {
